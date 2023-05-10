409507c5765c12fe864df52a3f94445db599eace
NaijaTechBro <bakisodiq@gmail.com>
2023-05-10 14:09:48 +0100

    auth --controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { getUserByEmail } from '../db/users';
 
 export const register = async (req: express.Request, res: express.Response) => {
     try {
+        const { email, password, username } = req.body;
+
+        if (!email || !password || !username) {
+            return res.sendStatus(400)
+        }
+
+        const exisitingUser = await getUserByEmail(email);
+
+        if (exisitingUser) {
+            return res.sendStatus(400);
+        }
         
     } catch (error) {
-        
+        console.log(error);
+        return res.sendStatus(400);
     }
 }
