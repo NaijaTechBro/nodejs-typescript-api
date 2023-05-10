21d9f1086f187a59739d4d2d6bd901c7c12e19f3
NaijaTechBro <bakisodiq@gmail.com>
2023-05-10 14:06:15 +0100

    auth --controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,3 +1,9 @@
 import express from 'express';
 
-export 
+export const register = async (req: express.Request, res: express.Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
