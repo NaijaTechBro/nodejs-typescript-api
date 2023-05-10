aaf19170e738f94c996d1220f70909f7cdb7b66a
NaijaTechBro <bakisodiq@gmail.com>
2023-05-10 14:20:15 +0100

    index

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
-import router from 
+import router from './router';
 
 
 const app = express()
@@ -32,3 +31,5 @@ const MONGO_URL = "mongodb+srv://nodejstypescript:DxjjoQ1jk644Nvee@cluster0.9zxq
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
